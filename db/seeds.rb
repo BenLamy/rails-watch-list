1aef4ccd539b234103264ba4ee11af4650e26745
Benoit Lamy <lamybenoit@gmail.com>
2024-11-14 17:10:44 +0100

    Adding routes, controllers and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+Movie.destroy_all
+
+puts 'Creating 10 fake movies'
+
+10.times do
+  movie = Movie.new(
+    title:   Faker::Movie.title,
+    overview: Faker::ChuckNorris.fact,
+    poster_url: Faker::LoremFlickr.image,
+    rating: Faker::Number.decimal(l_digits: 2)
+  )
+    movie.save!
+end
+puts 'Finished!'
